1d45be4d8b88206999224372847aa50b67e1d9b8
Ewelina Pawłowska <e.pawlowska.97@gmail.com>
2020-03-04 12:19:57 +0100

    Periods and print method added

---
+++ PeriodMerger/Period.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Periods
+{
+    class Period
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public Period(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    
+}
--- PeriodMerger/Program.cs
+++ PeriodMerger/Program.cs
@@ -1,11 +1,51 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace PeriodMerger
+namespace Periods
 {
     class Program
     {
         static void Main()
         {
+            var periods = new List<Period>
+            {
+                new Period (1, 5),
+                new Period (3, 5),
+                new Period (5, 10),
+                new Period (4, 8),
+                new Period (7, 13),
+                new Period (8, 15),
+                new Period (13, 20),
+                new Period (15, 25),
+                new Period (17, 20),
+                new Period (27, 30),
+                new Period (28, 29),
+                new Period (29, 30),
+                new Period (27, 29),
+                new Period (23, 25),
+                new Period (21, 29),
+                new Period (22, 25),
+                new Period (21, 28),
+                new Period (18, 20),
+                new Period (17, 27),
+                new Period (15, 25)
+            };
+            
+            var sortedPeriods = periods.OrderBy(i => i.Start).ToList();
+            
+            PrintPeriods(sortedPeriods);
+            
+        }
+
+        static void PrintPeriods(IEnumerable<Period> sortedPeriods)
+        {
+            Console.WriteLine("Sorted List with periods:");
+            foreach (var a in sortedPeriods)
+            {
+                Console.WriteLine($"Start: {a.Start} End: {a.End}");
+            }
             
         }
     }
